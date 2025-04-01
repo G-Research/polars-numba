1f5c1751c08ecc3242d2e662f84a38e79bbc3dd2
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-04-01 12:35:08 -0400

    Tweaks

--- examples_memory_usage.py
+++ examples_memory_usage.py
@@ -2,29 +2,27 @@
 Exposing Polars data to Numba is zero-copy, involving no extra memory usage.
 
 Creating a new Series involves 2x memory usage at peak because of the ArrayBuilder API.
+
+Note that the memory measurement technique may be underestimating peak memory usage...
 """
 
-from resource import getrusage, RUSAGE_SELF
-
 import psutil
+import gc
 import polars as pl
 from polars_numba import arrow_jit
 
 MB = 1_000_000
 
+
 def get_memory():
-    return psutil.Process().memory_full_info().uss
-
-
-def get_peak_memory():
-    return getrusage(RUSAGE_SELF).ru_maxrss * 1024  # kb on Linux
+    gc.collect()
+    return psutil.Process().memory_full_info().uss // MB
 
 
 # Series with two chunks:
-df = pl.DataFrame(
-    {"v": pl.concat([pl.Series(range(50_000_000)), pl.Series(range(50_000_000))])}
-)
+df = pl.DataFrame({"v": pl.concat([pl.Series(range(25_000_000)) for _ in range(4)])})
 print("DataFrame size in memory (MB):", df.estimated_size() // MB)
+
 
 @arrow_jit(returns_scalar=True)
 def mysum(arr):
@@ -34,13 +32,15 @@ def mysum(arr):
             result += value
     return result
 
-@arrow_jit(returns_scalar=False)
+
+@arrow_jit(returns_scalar=False, is_elementwise=True)
 def duplicate(arr, array_builder):
     for value in arr:
         if value is None:
             array_builder.null()
         else:
             array_builder.integer(value)
+
 
 # Precompile the Numba functions:
 small_df = pl.DataFrame({"v": pl.Series([1, 2, 3])})
@@ -49,21 +49,35 @@ small_df.select(duplicate(pl.col("v")))
 
 # Baseline memory usage:
 mem_start = get_memory()
-print("Baseline memory usage (MB):", mem_start // MB)
+print("Baseline memory usage (MB):", mem_start)
 
 # Pass Series to Arrow Numba JIT:
 mysum(df.select(mysum(pl.col("v"))))
 
 # Memory usage should be basically the same:
-print("Extra memory having called Numba with DataFrame -> scalar (MB):", (get_memory() - mem_start) / MB)
+print(
+    "Extra memory having called Numba with DataFrame -> scalar (MB):",
+    (get_memory() - mem_start),
+)
 mem_start = get_memory()
-peak_start = get_peak_memory()
 
 # Create new Series via Arrow Numba JIT:
 result = df.select(duplicate(pl.col("v")))
 
-# Extra peak allocated memroy is ...:
-print("Extra PEAK memory from using Arrow Numba JIT to duplicate Series (MB):", (get_peak_memory() - peak_start) // MB)
+# Extra allocated memory usage is 2×?! I am confused by why this isn't reflected in the peak though.
+print(
+    "Extra memory from using Arrow Numba JIT to duplicate Series (MB):",
+    (get_memory() - mem_start),
+)
+
+
+mem_start = get_memory()
+
+# Create new Series via Arrow Numba JIT using lazy + streaming:
+result3 = df.lazy().select(duplicate(pl.col("v"))).collect(engine="streaming")
 
 # Extra allocated memory usage is 1×:
-print("Extra FINAL memory from using Arrow Numba JIT to duplicate Series (MB):", (get_memory() - mem_start) // MB)
+print(
+    "Extra memory from from STREAMING is_elementwise=True using Arrow Numba JIT to duplicate Series (MB):",
+    (get_memory() - mem_start),
+)
