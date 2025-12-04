2140136e550d87702179333e75116709014bdb4e
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-12-04 10:25:19 -0500

    Switch to match.

--- src/polars_numba/__init__.py
+++ src/polars_numba/__init__.py
@@ -42,91 +42,126 @@ def collect_fold(
     """
     lazy_df = df.lazy().select_seq(*column_names).drop_nulls()
     numba_function = jit(nogil=True)(function)
-    num_columns = len(column_names)
 
-    if num_columns == 0:
-        raise ValueError("You must pass in at least one column name")
+    # As an alternative to doing dispatch here, we could do the dispatch inside
+    # the Numba function, which would be less verbose and duplicative. However,
+    # that means longer compilation time since the Numba function will be much
+    # more complex.
+    match len(column_names):
+        case 0:
+            raise ValueError("You must pass in at least one column name")
 
-    # As an alternative we could do the dispatch inside the Numba function.
-    # However, that means longer compilation time since the function will be
-    # much more complex.
-    if num_columns == 1:
+        case 1:
 
-        @jit(nogil=True)
-        def folder(acc, arr1):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1):
+                for i in range(len(arr1)):
+                    acc = numba_function(acc, arr1[i])
+                return acc
 
-    elif num_columns == 2:
+        case 2:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2):
+                for i in range(len(arr1)):
+                    acc = numba_function(acc, arr1[i], arr2[i])
+                return acc
 
-    elif num_columns == 3:
+        case 3:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3):
+                for i in range(len(arr1)):
+                    acc = numba_function(acc, arr1[i], arr2[i], arr3[i])
+                return acc
 
-    elif num_columns == 4:
+        case 4:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4):
+                for i in range(len(arr1)):
+                    acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i])
+                return acc
 
-    elif num_columns == 5:
+        case 5:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4, arr5):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4, arr5):
+                for i in range(len(arr1)):
+                    acc = numba_function(
+                        acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i]
+                    )
+                return acc
 
-    elif num_columns == 6:
+        case 6:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i], arr6[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6):
+                for i in range(len(arr1)):
+                    acc = numba_function(
+                        acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i], arr6[i]
+                    )
+                return acc
 
-    elif num_columns == 7:
+        case 7:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i], arr6[i], arr7[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7):
+                for i in range(len(arr1)):
+                    acc = numba_function(
+                        acc,
+                        arr1[i],
+                        arr2[i],
+                        arr3[i],
+                        arr4[i],
+                        arr5[i],
+                        arr6[i],
+                        arr7[i],
+                    )
+                return acc
 
-    elif num_columns == 8:
+        case 8:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i], arr6[i], arr7[i], arr8[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8):
+                for i in range(len(arr1)):
+                    acc = numba_function(
+                        acc,
+                        arr1[i],
+                        arr2[i],
+                        arr3[i],
+                        arr4[i],
+                        arr5[i],
+                        arr6[i],
+                        arr7[i],
+                        arr8[i],
+                    )
+                return acc
 
-    elif num_columns == 9:
+        case 9:
 
-        @jit(nogil=True)
-        def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9):
-            for i in range(len(arr1)):
-                acc = numba_function(acc, arr1[i], arr2[i], arr3[i], arr4[i], arr5[i], arr6[i], arr7[i], arr8[i], arr9[i])
-            return acc
+            @jit(nogil=True)
+            def folder(acc, arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9):
+                for i in range(len(arr1)):
+                    acc = numba_function(
+                        acc,
+                        arr1[i],
+                        arr2[i],
+                        arr3[i],
+                        arr4[i],
+                        arr5[i],
+                        arr6[i],
+                        arr7[i],
+                        arr8[i],
+                        arr9[i],
+                    )
+                return acc
 
-    else:
-        raise RuntimeError(
-            f"You passed in {num_columns} columns, but currently only up to 9"
-            " columns are supported; if you need more, file an issue"
-        )
+        case _:
+            raise RuntimeError(
+                f"You passed in {len(column_names)} columns, but currently only up to 9"
+                " columns are supported; if you need more, file an issue"
+            )
 
     acc = initial_accumulator
     for batch_df in lazy_df.collect_batches(chunk_size=50_000):
