6abc62d33f9ddb0fcd15d3e99ea104fbbb7928c0
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-12-12 08:57:34 -0500

    Another example

--- examples_fold.py
+++ examples_fold.py
@@ -8,10 +8,10 @@ import polars as pl
 
 from polars_numba import collect_fold
 
-### Longest streak of days below freezing
+#############################################
+### Longest streak of days below freezing ###
 
 # We have the daily min and max temperature, in Celsius:
-
 df = pl.DataFrame(
     {
         "date": pl.date_range(date(2025, 1, 1), date(2025, 1, 9), eager=True),
@@ -40,3 +40,40 @@ assert streak == 3
 # don't actually have to specificy column names:
 streak, _ = collect_fold(df, (0, 0), freezing_streak)
 assert streak == 3
+
+
+#########################################
+### Calculating a credit card balance ###
+
+
+def credit_card_balance(
+    starting_balance: float, max_allowed_balance: float, attempted_purchases: pl.Series
+) -> float:
+    """
+    Given a starting balance, a maximum allowed balance, and a series of
+    attempted purchase amounts, return the final balance.
+
+    Any purchase that takes the balance above the maximum allowed balance will
+    be rejected.
+    """
+
+    # We pass max_allowed_balance in through the accumuluator, since for
+    # performance reasons changing a function's bound variable is not allowed.
+    def maybe_sum(acc, attempted_purchase):
+        current_balance, max_allowed_balance = acc
+        new_balance = current_balance + attempted_purchase
+        if new_balance <= max_allowed_balance:
+            current_balance = new_balance
+        return (current_balance, max_allowed_balance)
+
+    df = pl.DataFrame({"attempted_purchase": attempted_purchases})
+    final_balance, _ = collect_fold(
+        df, (starting_balance, max_allowed_balance), maybe_sum
+    )
+    return final_balance
+
+attempted_purchases = pl.Series([900, 70, -400, 60])
+final_balance = credit_card_balance(50, 1000, attempted_purchases)
+# We expect the 70 purchase to be rejected because it will take the balance
+# above 1000:
+assert final_balance == 50 + 900 - 400 + 60
